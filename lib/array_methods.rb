870211be459f76c5382e03f049e2b29c45c15118
Zach Brown <zachmb22@gmail.com>
2020-01-20 20:44:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,9 +18,9 @@ end
 
 def find_min_value(array)
   minnum = nil
-  array.length.times do |minnum|
-    if num < minnum
-      minnum = num
+  array.length.times do |count|
+    if array[count] < minnum
+      minnum = array[count]
     end
   end
   minnum
